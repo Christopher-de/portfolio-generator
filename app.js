4bc75b9764a8c9571e7261dc93710f96abad85bc
Get Shwifty <cjpatr0n1te@icloud.com>
2021-08-05 12:13:01 -0700

    html generation

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+
 const profileDataArgs = process.argv.slice(2);
 
-const printProfileData = profileDataArr => {
-    //this
-    for (let i=0; i<profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
+console.log(profileDataArgs);
 
-    console.log('===========');
-    
-    //is the same as this
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+const [name, github] = profileDataArgs;
 
-printProfileData(profileDataArgs);
+console.log(name, github);
+
+const pageHTML = generatePage(name, github);
+
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
